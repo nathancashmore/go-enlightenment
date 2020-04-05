2b49c9c816e43cfa5935711568a55c697bfef645
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-04-05 08:31:00 +0100

    Add more meaningful godoc for packages

--- hello/hello.go
+++ hello/hello.go
@@ -1,4 +1,15 @@
-package main
+/*
+Hello (name, language) :
+Provides examples of the following:
+ import - e.g. fmt and os
+ func - public & private (uppercase & lowercase)
+ const - public & private (uppercase & lowercase)
+ if
+ switch
+ returning declared variable
+ processing input arguments
+*/
+package hello
 
 import (
 	"fmt"
@@ -25,6 +36,12 @@ func main() {
 	fmt.Println(Hello(name, lang))
 }
 
+// Provide a greeting for a given name in a number
+// of different languages
+//
+// Supported Languages:
+//
+//  [ "English", "French", "Spanish" ]
 func Hello(name string, lang string) string {
 	if name == "" {
 		name = defaultGreeting
--- integers/adder.go
+++ integers/adder.go
@@ -1,5 +1,11 @@
+/*
+Add( x, y ) :
+Provides examples of the following:
+ int
+*/
 package integers
 
+// Add two integers together
 func Add(x, y int) int {
 	return x + y
 }
