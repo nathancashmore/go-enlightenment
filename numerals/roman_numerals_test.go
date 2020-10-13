6386ee7d25b6729f516e74a4c9bf8107ec9fe0a8
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-10-13 21:35:34 +0100

    Simplify test name and add additional scenarios for higher numerals

--- numerals/roman_numerals_test.go
+++ numerals/roman_numerals_test.go
@@ -1,38 +1,51 @@
 package numerals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 
 	t.Run("should return correct roman numeral for input", func(t *testing.T) {
 		testinputs := []struct {
-			description string
-			decimal     int
-			numeral     string
+			decimal int
+			numeral string
 		}{
-			{"convert 1 -> I", 1, "I"},
-			{"convert 2 -> II", 2, "II"},
-			{"convert 3 -> III", 3, "III"},
-			{"convert 4 -> IV", 4, "IV"},
-			{"convert 5 -> V", 5, "V"},
-			{"convert 6 -> VI", 6, "VI"},
-			{"convert 7 -> VII", 7, "VII"},
-			{"convert 8 -> VIII", 8, "VIII"},
-			{"convert 9 -> IX", 9, "IX"},
-			{"convert 10 -> X", 10, "X"},
-			{"10 gets converted to X", 10, "X"},
-			{"14 gets converted to XIV", 14, "XIV"},
-			{"18 gets converted to XVIII", 18, "XVIII"},
-			{"20 gets converted to XX", 20, "XX"},
-			{"39 gets converted to XXXIX", 39, "XXXIX"},
-			{"40 gets converted to XL", 40, "XL"},
-			{"47 gets converted to XLVII", 47, "XLVII"},
-			{"49 gets converted to XLIX", 49, "XLIX"},
-			{"50 gets converted to L", 50, "L"},
+			{1, "I"},
+			{2, "II"},
+			{3, "III"},
+			{4, "IV"},
+			{5, "V"},
+			{6, "VI"},
+			{7, "VII"},
+			{8, "VIII"},
+			{9, "IX"},
+			{10, "X"},
+			{10, "X"},
+			{14, "XIV"},
+			{18, "XVIII"},
+			{20, "XX"},
+			{39, "XXXIX"},
+			{40, "XL"},
+			{47, "XLVII"},
+			{49, "XLIX"},
+			{50, "L"},
+			{100, "C"},
+			{90, "XC"},
+			{400, "CD"},
+			{500, "D"},
+			{900, "CM"},
+			{1000, "M"},
+			{1984, "MCMLXXXIV"},
+			{3999, "MMMCMXCIX"},
+			{2014, "MMXIV"},
+			{1006, "MVI"},
+			{798, "DCCXCVIII"},
 		}
 
 		for _, test := range testinputs {
-			t.Run(test.description, func(t *testing.T) {
+			t.Run(fmt.Sprintf("decimal value %d is converted to numeral %q", test.decimal, test.numeral), func(t *testing.T) {
 				result := convert(test.decimal)
 				if result != test.numeral {
 					t.Errorf("expected %q, got %q", test.numeral, result)
