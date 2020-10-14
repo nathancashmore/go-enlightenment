2a61f0422b0e386a8479e17585597d484c67f556
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-10-14 19:34:37 +0100

    Addition of reverse conversion from Numeral to integer

--- numerals/roman_numerals.go
+++ numerals/roman_numerals.go
@@ -1,41 +1,86 @@
 package numerals
 
-import "strings"
+import (
+	"strings"
+)
 
-func convert(value int) string {
+type RomanNumeral struct {
+	Integer int
+	Numeral string
+}
 
-	type RomanNumeral struct {
-		Decimal int
-		Numeral string
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, numeral := range allRomanNumerals {
+		if numeral.Numeral == symbol {
+			return numeral.Integer
+		}
 	}
+	return 0
+}
 
-	var allRomanNumerals = []RomanNumeral{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
-	}
+var allRomanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
 
+func convertToNumeral(value int) string {
 	var result strings.Builder
 	total := value
 
 	for total > 0 {
 		for _, value := range allRomanNumerals {
-			if total > value.Decimal-1 {
+			if total > value.Integer-1 {
 				result.WriteString(value.Numeral)
-				total -= value.Decimal
+				total -= value.Integer
 				break
 			}
 		}
 	}
 	return result.String()
+}
+
+func convertToInteger(numerals string) int {
+	var total = 0
+	var symbol strings.Builder
+
+	for len(numerals) > 0 {
+		var value = 0
+		symbol.Reset()
+
+		if len(numerals) > 1 {
+			// First find a pair and check if they return a value
+			symbol.WriteString(string(numerals[0]))
+			symbol.WriteString(string(numerals[1]))
+
+			value = allRomanNumerals.ValueOf(symbol.String())
+		}
+
+		if value == 0 {
+			// Pair was not found so try a single value
+			symbol.Reset()
+			symbol.WriteString(string(numerals[0]))
+
+			value = allRomanNumerals.ValueOf(symbol.String())
+		}
+
+		total += value
+
+		//Remove the symbol found from the numerals
+		numerals = numerals[symbol.Len():]
+	}
+
+	return total
 }
--- numerals/roman_numerals_test.go
+++ numerals/roman_numerals_test.go
@@ -6,49 +6,59 @@ import (
 )
 
 func TestRomanNumerals(t *testing.T) {
+	testinputs := []struct {
+		integer int
+		numeral string
+	}{
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{100, "C"},
+		{90, "XC"},
+		{400, "CD"},
+		{500, "D"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLXXXIV"},
+		{3999, "MMMCMXCIX"},
+		{2014, "MMXIV"},
+		{1006, "MVI"},
+		{798, "DCCXCVIII"},
+	}
 
-	t.Run("should return correct roman numeral for input", func(t *testing.T) {
-		testinputs := []struct {
-			decimal int
-			numeral string
-		}{
-			{1, "I"},
-			{2, "II"},
-			{3, "III"},
-			{4, "IV"},
-			{5, "V"},
-			{6, "VI"},
-			{7, "VII"},
-			{8, "VIII"},
-			{9, "IX"},
-			{10, "X"},
-			{10, "X"},
-			{14, "XIV"},
-			{18, "XVIII"},
-			{20, "XX"},
-			{39, "XXXIX"},
-			{40, "XL"},
-			{47, "XLVII"},
-			{49, "XLIX"},
-			{50, "L"},
-			{100, "C"},
-			{90, "XC"},
-			{400, "CD"},
-			{500, "D"},
-			{900, "CM"},
-			{1000, "M"},
-			{1984, "MCMLXXXIV"},
-			{3999, "MMMCMXCIX"},
-			{2014, "MMXIV"},
-			{1006, "MVI"},
-			{798, "DCCXCVIII"},
-		}
-
+	t.Run("should return correct roman numeral for associated integer", func(t *testing.T) {
 		for _, test := range testinputs {
-			t.Run(fmt.Sprintf("decimal value %d is converted to numeral %q", test.decimal, test.numeral), func(t *testing.T) {
-				result := convert(test.decimal)
+			t.Run(fmt.Sprintf("integer value %d is converted to numeral %q", test.integer, test.numeral), func(t *testing.T) {
+				result := convertToNumeral(test.integer)
 				if result != test.numeral {
 					t.Errorf("expected %q, got %q", test.numeral, result)
+				}
+			})
+		}
+	})
+
+	t.Run("should return correct integer for associated roman numeral", func(t *testing.T) {
+		for _, test := range testinputs {
+			t.Run(fmt.Sprintf("numeral value %q is converted to integer %d", test.numeral, test.integer), func(t *testing.T) {
+				result := convertToInteger(test.numeral)
+				if result != test.integer {
+					t.Errorf("expected %d, got %d", test.integer, result)
 				}
 			})
 		}
